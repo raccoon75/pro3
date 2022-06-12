be7c85a691f243bb9cafa7f237c37efdaaa71977
raccoon75 <104978768+raccoon75@users.noreply.github.com>
2022-06-12 13:55:19 +0300

    Update att3.py

--- att3.py
+++ att3.py
@@ -213,7 +213,6 @@ with st.echo(code_location='below'):
     c = conn.cursor()
     teslasqldf = pd.read_csv('https://raw.githubusercontent.com/oubielamir/Tesla-Stock-Analysis/master/TSLA.csv')
     teslasqldf = teslasqldf.rename(columns={'Adj Close': 'AdjClose'})
-    teslasql = teslasqldf.to_sql('tesla', conn)
     tesladf = pd.read_sql(
         """
     SELECT Date, Close FROM tesla
